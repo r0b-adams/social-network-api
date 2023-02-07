f7cd73a6fd1d315f3ffd86bc6f7c64975064b75a
Robert Adams <adamsiii.robert@gmail.com>
2023-02-06 18:39:49 -0800

    add server port env var

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,6 @@ const config_path = path.join(__dirname, "/.env");
 dotenv.config({ path: config_path });
 
 export const CONFIG = {
+  SERVER_PORT: process.env.PORT || 3050,
   ENV: process.env.ENVIRONMENT,
-  TEST: process.env.TEST,
 };
