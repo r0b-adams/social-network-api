190dbdda1b51c0629ce3ec4414ba10ca6d8b128a
Robert Adams <adamsiii.robert@gmail.com>
2023-02-08 18:33:40 -0800

    Add MONGODB_URI to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,11 @@
-import * as dotenv from "dotenv";
-import path from "path";
+import * as dotenv from 'dotenv';
+import path from 'path';
 
-const config_path = path.join(__dirname, "/.env");
+const config_path = path.join(__dirname, '/.env');
 dotenv.config({ path: config_path });
 
 export const CONFIG = {
-  SERVER_PORT: process.env.PORT || 3050,
-  ENV: process.env.ENVIRONMENT,
+  ENV: process.env.ENVIRONMENT || 'development',
+  SERVER_PORT: process.env.PORT || 3001,
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/social-network-api-db',
 };
