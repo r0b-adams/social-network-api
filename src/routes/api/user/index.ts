54148fb64005edc0f1fb1ffcabc8298a1d7fb342
Robert Adams <adamsiii.robert@gmail.com>
2023-02-08 18:56:08 -0800

    Factor out user handlers

---
+++ src/routes/api/user/index.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import * as users from './user-handlers';
+
+const router = Router();
+
+/**
+ * /api/users
+ */
+router.get('/', users.getAllUsers);
+router.get('/:_id', users.getOneUser);
+router.post('/', users.createUser);
+router.put('/:_id', users.updateUser);
+router.delete('/:_id');
+router.route('/:user_id/friends/:friend_id').post(users.addFriend).delete(users.removeFriend);
+
+export { router as userRoutes };
