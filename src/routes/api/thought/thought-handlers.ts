161cbd695743ef4d331c97003e599a5fb70be212
Robert Adams <adamsiii.robert@gmail.com>
2023-02-09 11:39:17 -0800

    Finish up request handlers

--- src/routes/api/thought/thought-handlers.ts
+++ src/routes/api/thought/thought-handlers.ts
@@ -1,20 +1,16 @@
 import { RequestHandler } from 'express';
 import { ParamsDictionary } from 'express-serve-static-core';
 
+import { IThought, IReaction } from '../../../models/Thought';
 import thoughtController from './ThoughtController';
 
+// alias
 type IParams = ParamsDictionary;
-
-interface IThought {
-  user_id: string;
-  username: string;
-  thought_text: string;
-}
 
 export const getAllThoughts: RequestHandler = async (_req, res, next) => {
   try {
     const thoughts = await thoughtController.getAllThoughts();
-    res.json(thoughts);
+    res.json({ thoughts });
   } catch (error) {
     next(error);
   }
@@ -24,38 +20,60 @@ export const getOneThought: RequestHandler = async (req, res, next) => {
   try {
     const { _id } = req.params;
     const thought = await thoughtController.getOneThought(_id);
-    res.json(thought);
+    res.json({ thought });
   } catch (error) {
     next(error);
   }
 };
 
-// POST to create a new thought
-// (don't forget to push the created thought's _id to the associated user's thoughts array field)
-export const createThought: RequestHandler<{}, {}, IThought> = async (req, res, next) => {
+export const createThought: RequestHandler<{}, {}, IThought & { user_id: string }> = async (req, res, next) => {
   try {
     const { user_id, username, thought_text } = req.body;
-    const thought = await thoughtController.createThought(user_id, username, thought_text);
-    res.json(thought);
+    const result = await thoughtController.createThought(user_id, username, thought_text);
+    res.json({ result });
   } catch (error) {
     next(error);
   }
 };
 
-// PUT to update a thought by its _id
 export const updateThought: RequestHandler<IParams, {}, IThought> = async (req, res, next) => {
   try {
     const { _id } = req.params;
     const { thought_text } = req.body;
-    const thought = await thoughtController.updateThought(_id, thought_text);
-    res.json(thought);
+    const thought = await thoughtController.updateThought(_id, { thought_text });
+    res.json({ thought });
   } catch (error) {
     next(error);
   }
 };
 
-// DELETE
+export const deleteThought: RequestHandler = async (req, res, next) => {
+  try {
+    const { _id } = req.params;
+    const result = await thoughtController.deleteThought(_id);
+    res.json({ result });
+  } catch (error) {
+    next(error);
+  }
+};
 
-// POST to create a reaction to be stored in a single thought's reactions array field
+export const addReaction: RequestHandler<IParams, {}, IReaction> = async (req, res, next) => {
+  try {
+    const { thought_id } = req.params;
+    const { username, reaction_body } = req.body;
+    const thought = await thoughtController.addReaction(thought_id, { username, reaction_body });
+    res.json({ thought });
+  } catch (error) {
+    next(error);
+  }
+};
 
-// DELETE to pull and remove a reaction by the reaction's reaction_id value
+export const removeReaction: RequestHandler = async (req, res, next) => {
+  try {
+    const { thought_id, reaction_id } = req.params;
+    const thought = await thoughtController.removeReaction(thought_id, reaction_id);
+    res.json({ thought });
+  } catch (error) {
+    next(error);
+  }
+};
