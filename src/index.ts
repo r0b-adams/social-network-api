5438f3647263b31a531447395c3726151dbcd45a
Robert Adams <adamsiii.robert@gmail.com>
2023-02-08 18:32:27 -0800

    Apply middleware & connect mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
 
-import { CONFIG } from "./config";
-import { routes } from "./routes";
+import { CONFIG } from './config';
+import { routes } from './routes';
+import { errorHandler } from './utils';
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
-app.listen(CONFIG.SERVER_PORT, () => {
-  console.log(`server listening on ${CONFIG.SERVER_PORT}`);
-});
+const connectDatabase = async () => {
+  try {
+    mongoose.set('strictQuery', false); // hide DeprecationWarning
+    await mongoose.connect(CONFIG.MONGODB_URI);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const startServer = async () => {
+  await connectDatabase();
+
+  app.listen(CONFIG.SERVER_PORT, () => {
+    console.log(`server listening on ${CONFIG.SERVER_PORT}`);
+  });
+};
+
+startServer();
