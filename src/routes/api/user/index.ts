c35fd9e911c7c44624eb321aca6a0f4c314ac0c5
Robert Adams <adamsiii.robert@gmail.com>
2023-02-09 11:39:37 -0800

    Add deleteUser handler

--- src/routes/api/user/index.ts
+++ src/routes/api/user/index.ts
@@ -4,13 +4,16 @@ import * as users from './user-handlers';
 const router = Router();
 
 /**
+ *
  * /api/users
+ *
  */
+
 router.get('/', users.getAllUsers);
 router.get('/:_id', users.getOneUser);
 router.post('/', users.createUser);
 router.put('/:_id', users.updateUser);
-router.delete('/:_id');
+router.delete('/:_id', users.deleteUser);
 router.route('/:user_id/friends/:friend_id').post(users.addFriend).delete(users.removeFriend);
 
 export { router as userRoutes };
