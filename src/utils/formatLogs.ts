8ce7fd49cc2fed5f33a7e3beb654bc8ca3d9fea3
Robert Adams <adamsiii.robert@gmail.com>
2023-02-11 20:12:42 -0800

    Add function to validate hex value, add set trap

--- src/utils/formatLogs.ts
+++ src/utils/formatLogs.ts
@@ -3,6 +3,10 @@ import { COLOR_HEXES, FORMAT } from './enums';
 type ColorProxy = typeof COLOR_HEXES & { [key: string | symbol]: string };
 type HexadecimalArray = RegExpMatchArray | string[] | null;
 
+// YYY, #YYY, YYYYYY, #YYYYYY
+const VALID_HEX = /(^#?[0-9A-F]{3}$)|(^#?[0-9A-F]{6}$)/gim;
+const validHex = (hex: string) => VALID_HEX.test(hex);
+
 // get array of RGB ints from a hexadecimal color
 const getRGB = (hexStr: string): number[] => {
   let hexadecimals: HexadecimalArray = null;
@@ -26,27 +30,31 @@ const getRGB = (hexStr: string): number[] => {
   throw new Error('invalid hex string format');
 };
 
-const VALIDATE = /(^#?[0-9A-F]{3}$)|(^#?[0-9A-F]{6}$)/gim;
-const validHex = (hex: string) => VALIDATE.test(hex);
-
 export const TEXT = new Proxy<ColorProxy>(COLOR_HEXES, {
-  // trap getters and return ANSI color escape codes
-  get: (target, key: string): string => {
+  // intercept getters to return ANSI color escape codes
+  get: (target, key: string): string | void => {
     if (!Reflect.has(target, key)) {
-      return FORMAT.RESET;
+      return;
     }
     const hex = target[key];
     const [R, G, B] = getRGB(hex);
     const code = '\x1b[38;2;' + R + ';' + G + ';' + B + 'm';
     return code;
   },
+  // save a hexadecimal color value at given key
+  // if key exists, overwrites value
   set: (target, key, value) => {
-    console.log(target[key]);
-    return Reflect.set(target, key, value);
+    if (validHex(value)) {
+      return Reflect.set(target, key, value);
+    }
+    // if invalid hex, return false
+    return false;
   },
 });
 
+console.log(TEXT.HOT_PINK);
 TEXT.HOT_PINK = '#ff69b4';
-console.log(TEXT.HOT_PINK, 'pink');
+const { HOT_PINK } = TEXT;
+console.log({ HOT_PINK });
 
 export { COLOR_HEXES as HEX, FORMAT };
