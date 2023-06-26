4560ef1c2a8039299af22db4a01d5cdd4a909a48
Robert Adams <adamsiii.robert@gmail.com>
2023-06-26 16:45:30 -0700

    update local connection string

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,5 +7,5 @@ dotenv.config({ path: config_path });
 export const CONFIG = {
   ENV: process.env.ENVIRONMENT || 'development',
   SERVER_PORT: process.env.PORT || 3001,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/social-network-api-db',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1/social-network-api-db',
 };
